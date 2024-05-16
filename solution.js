f5f86d26d3bcf6b882b36ade6e3a63090e4973f9
muthonijulie <julius.muthoni22@students.dkut.ac.ke>
2024-05-16 08:54:01 +0300

    update 15th js code

--- solution.js
+++ solution.js
@@ -1,41 +1,33 @@
 function cancellable(fn,args,t){
-    fn(...args);
-    let timer = setInterval(()=> fn(...args),t);
-
-    let cancelFn = () => clearInterval(timer)
-    return cancelFn;
-     }
-     const start = performance.now();
-     
-       const log = (...argsArr) => {
-           const diff = Math.floor(performance.now() - start);
-           result.push({"time": diff, "returned": fn(...argsArr)});
-       }
+   console.log(fn(...args));
+    const intervalId=setInterval(()=>{console.log(fn(...args));},t);
+    return function cancelFn(){
+        clearInterval(intervalId);
+    };
+}
+   
  //example 1
-//  const fn = (x) => {
-//     const result = x * 2;
-//     console.log(result);
-//     return result;
-// };
+//  const fn = (x) =>  x * 2;
+    
 //  const cancelFn=cancellable(fn,[4], 35);
 //  setTimeout(cancelFn, 190);
 
 //example 2
-// const  fn = (x1, x2) => {
-//     const result =x1 * x2;
-//     console.log(result);
-//     return result;
-// };
+// const  fn = (x1, x2) => x1 * x2;
+    
 // const cancelFn=cancellable(fn, [2, 5], 30);
 //  setTimeout(cancelFn, 150);
 
  //example 3
- const  fn = (x1, x2, x3)  => {
-    const result = x1 + x2 + x3;
-    console.log(result);
-    return result;
-};
+ const  fn = (x1, x2, x3)  => x1 + x2 + x3;
+    
 const cancelFn=cancellable(fn,  [5, 1, 3], 50);
  setTimeout(cancelFn, 180);
 
+ //setInterval
+ function sayHello(name){
+    console.log("Hello" +name);
+ }
+ const intervalId=setInterval(sayHello,2000,"Muthoni");
+setTimeout(()=>{clearInterval(intervalId);},8000);
  
